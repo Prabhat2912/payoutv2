8878fe2e2d488ce2645a9437010a86260da41ead
Prabhat2912 <pk993105@gmail.com>
2024-01-10 00:52:55 +0530

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="flex flex-row px-[32px] py-[12px] gap-x-[16px]  justify-start items-center border-2 border-gray-200 w-[1275px]">
       <h1>Payments</h1>
       <div className="flex flex-row gap-x-[6px] w-full ">
-        <img src="/public/Help.png" alt="" />
+        <img src="/Help.png" alt="" />
         <p className="text-[12px]">How it works</p>
       </div>
       <div className="flex flex-row  items-center px-[16px] py-[9px] bg-gray-100 space-x-2 rounded-md   ">
